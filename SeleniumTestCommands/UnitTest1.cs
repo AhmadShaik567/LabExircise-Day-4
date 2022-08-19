7d0404cdc2f0ee057501a84a19c7d67c717f4307
AhmadShaik567 <ahmadshaik.qt@gmail.com>
2022-08-19 17:56:58 +0530

    first commit

---
+++ SeleniumTestCommands/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SeleniumTestCommands
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
